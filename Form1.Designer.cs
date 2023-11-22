213c1eb5317ecf6e5ff61208740796ae34c4b083
Veronika <vrnkdksha@gmail.com>
2023-11-22 18:34:24 +0300

    каааа

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,12 +33,14 @@
             // 
             // button1
             // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button1.AutoSize = true;
             this.button1.ForeColor = System.Drawing.Color.Black;
             this.button1.Image = global::Hog_Jumper.Properties.Resources.Png;
             this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button1.Location = new System.Drawing.Point(188, 51);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(435, 364);
+            this.button1.Size = new System.Drawing.Size(435, 426);
             this.button1.TabIndex = 0;
             this.button1.Text = "да";
             this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -53,7 +55,9 @@
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
