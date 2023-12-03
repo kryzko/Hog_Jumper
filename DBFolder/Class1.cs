d7ad880783baee65a3bcb9cd8c44c2a6b4079cc6
Veronika <vrnkdksha@gmail.com>
2023-12-03 18:19:20 +0300

    делаю регистрацию добавила классы

---
+++ DBFolder/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+
+namespace Hog_Jumper.DBFolder
+{
+    internal class Class1
+    {
+        OleDbConnection conn;
+        OleDbCommand cmd;
+    }
+}
---
+++ DBFolder/ConnectionString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Hog_Jumper.DBFolder
+{
+    internal class ConnectionString
+    {
+        public static string ConnectStr
+        {
+            get
+            {
+                return ConfigurationManager.ConnectionStrings["Hog_Jumper.Properties.Settings.ConnectionString"].ConnectionString;
+            }
+        }
+    }
+}
